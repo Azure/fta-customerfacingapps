ea07ffa6a58b9fc82e31c77394f65953e4d910e1
Jelle Druyts <jelle_druyts@yahoo.com>
2017-09-12 12:13:26 +0200

    Added Application Insights support for web app. Fixes #1505.

--- Relecloud.Web/Program.cs
+++ Relecloud.Web/Program.cs
@@ -12,6 +12,7 @@ namespace Relecloud.Web
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
