84f8f0df05b5044c87d97320feceac7b561c653a
Ken Schlobohm <keschlob@microsoft.com>
2022-03-15 11:23:02 -0500

    updated azure search package

--- ecommerce/src/Relecloud.Web/Infrastructure/ExtensionMethods.cs
+++ ecommerce/src/Relecloud.Web/Infrastructure/ExtensionMethods.cs
@@ -99,5 +99,23 @@ namespace Relecloud.Web.Infrastructure
             }
             return html.ActionLink(linkText, "Search", "Concert", null, null, null, routeValues, null);
         }
+
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (list is List<T> asList)
+            {
+                asList.AddRange(items);
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    list.Add(item);
+                }
+            }
+        }
     }
 }
